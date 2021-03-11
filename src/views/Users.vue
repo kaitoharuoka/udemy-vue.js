e81591de50854221019e0a415fafa3ea8f192f31
kaitoharuoka <kaitokaito54@gmail.com>
2021-03-11 19:51:46 +0900

    commit12

---
+++ src/views/Users.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>Users</h3>
+    <router-link to="/user/1">ユーザー１</router-link>
+    <router-link to="/user/2">ユーザー２</router-link>
+    <hr>
+    <h1>User No. {{ $route.params.id }}</h1>
+  </div>
+</template>
+
